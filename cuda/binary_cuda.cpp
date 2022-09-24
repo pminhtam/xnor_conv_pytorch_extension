b11d5144611a2426ffc0c39f0a29282c4b2d1e85
Phạm Minh Tâm <v.tampm2@vinai.io>
2022-09-24 12:59:52 +0700

    cuda : dell all redundant comment

--- cuda/binary_cuda.cpp
+++ cuda/binary_cuda.cpp
@@ -1,10 +1,6 @@
 #include <torch/extension.h>
-//#include <torch/types.h>
 #include <vector>
-//#include <iostream>
-//#include <cuda.h>
-//#include <cuda_runtime.h>
-// #include "binop_cuda_kernel.cu"
+
 #define ENCODE_BITS 16
 
 torch::Tensor encode_rows(torch::Tensor input,int is_filter_transpose);
@@ -29,8 +25,6 @@ torch::Tensor binary_conv2d(
 
     const int batch_size = input.size(0), c = input.size(1), h = input.size(2), w = input.size(3);
     const int c_out = weights.size(0), c_in = weights.size(1), k1 = weights.size(2), k2 = weights.size(3);
-//    auto X = 2*input;
-    // auto input2 = input.to(torch::kFloat32);
     torch::Tensor col_mat = torch::im2col(input,/*kernel_size=*/torch::IntArrayRef({3, 3}),
                                                  /*dilation=*/torch::IntArrayRef({1, 1}),
                                                  /*padding=*/torch::IntArrayRef({0, 0}),
@@ -53,22 +47,13 @@ torch::Tensor binary_conv2d(
     torch::Tensor out_tensor = torch::zeros(torch::IntArrayRef({batch_size,c_out,n}),torch::TensorOptions()
                     .dtype(torch::kInt32).device(torch::kCUDA, 0));
     fil_pack = encode_rows(bin_fil,1);
-    // torch::Tensor col_pack = torch::zeros(torch::IntArrayRef({batch_size,n,l}));
-    // std::cout<< "zzzzzzzzzzzzzzz " << '\n';
   std::cout << fil_pack.size(0) << "  ,   " << fil_pack.size(1) << '\n';
 
    for(idx = 0; idx < batch_size; idx++){
-//    torch::Tensor col_pack = encode_rows_cpu(bin_col[0]);
        col_pack[idx] = encode_rows(bin_col[idx],0);
-//       std::cout << col_pack[idx].size(0) << "  ,   " << col_pack[idx].size(1) << '\n';
        out_tensor[idx] = binary_gemm(fil_pack, col_pack[idx], c_out,l,k,n,0,1,1);
-      //  std::cout << idx ;
-      // std::cout << idx << col_pack[idx] << '\n' ;
-
    }
     return  out_tensor;
-    // return  fil_pack;
-
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
--- cuda/binop_cuda_kernel.cu
+++ cuda/binop_cuda_kernel.cu
@@ -18,7 +18,6 @@ int GET_BLOCKS(int N){
 
 __device__ __forceinline__ int encode_val(float* array, int remain_bit,int n,int is_filter_transpose) {
     int r = 0;
-    // float r2 = 0;
     for(int i=0; i<ENCODE_BITS && i<remain_bit; i++){
         if (is_filter_transpose){
             r |= (array[i]>0)<<i;
@@ -27,7 +26,6 @@ __device__ __forceinline__ int encode_val(float* array, int remain_bit,int n,int
             r |= (array[i*n]>0)<<i;
         }
     }
-    // r2 = r;
     return r;
 }
 //  Chuyển float32 -> bit
@@ -38,11 +36,9 @@ __global__ void encode_rows_kernel(float *input, int* output, int n, int k, int 
     int l_idx = blockIdx.y;
     int start_bit = l_idx*ENCODE_BITS;
     int remain_bit = k - start_bit;
-//     int p = n*(i/l)+ENCODE_BITS*(i%l);
     if (n_idx<n & l_idx<l & start_bit<k & remain_bit>0) {
         if (is_filter_transpose) {
             output[l_idx + n_idx*l] = encode_val(&input[start_bit + n_idx*k ], remain_bit,n,is_filter_transpose);
-//         output[l_idx + n_idx*l] = encode_val(&input[n_idx + start_bit*n_idx ], remain_bit,n);
         }
         else{
             output[l_idx + n_idx*l] = encode_val(&input[n_idx + start_bit*n ], remain_bit,n,is_filter_transpose);
@@ -51,7 +47,6 @@ __global__ void encode_rows_kernel(float *input, int* output, int n, int k, int 
 }
 
 torch::Tensor encode_rows(torch::Tensor input,int is_filter_transpose) {
-    //THCUNN_assertSameGPU(state, 2, input, output);
 
 // chuyển float32 sang binary
     int n = input.size(0);  // = h*w
@@ -59,16 +54,12 @@ torch::Tensor encode_rows(torch::Tensor input,int is_filter_transpose) {
     int l = 1+(k-1)/ENCODE_BITS;
 
     torch::Tensor output = torch::zeros(torch::IntArrayRef({n,l}),torch::TensorOptions().dtype(torch::kInt32).device(torch::kCUDA, 0));
-    // torch::Tensor output = torch::zeros(torch::IntArrayRef({n,l}));
 
     auto a = input.data_ptr<float>();
     auto b = output.data_ptr<int>();
 
-//    encode_rows_cuda(a, b, n, k, l);
-//   const int threads = CUDA_NUM_THREADS;
-//   const dim3 blocks((state_size + threads - 1) / threads, batch_size);
+
   const int threads = 1024;
-//   const dim3 threads(1024,);
   const dim3 blocks(n/1024 + 1 ,l);
 
     AT_DISPATCH_FLOATING_TYPES(input.type(), "encode_rows", ([&] {
@@ -79,53 +70,22 @@ torch::Tensor encode_rows(torch::Tensor input,int is_filter_transpose) {
    return output;
 }
 
-// void encode_rows_cuda(float* input, int* output, int m, int n, int l) {
-// //     encode_rows_kernel <<< GET_BLOCKS(m*l), CUDA_NUM_THREADS, 0, stream >>>(input, output, m, n, l);
-//     AT_DISPATCH_FLOATING_TYPES
-// }
+
 template <typename scalar_t>
 __global__ void binary_gemm_kernel(
         const torch::PackedTensorAccessor32<scalar_t,2,torch::RestrictPtrTraits> a,
         const torch::PackedTensorAccessor32<scalar_t,2,torch::RestrictPtrTraits> b,
         torch::PackedTensorAccessor32<scalar_t,3,torch::RestrictPtrTraits> c,
         int c_out , int l,int k, int n) {
-        // ta se tính theo  b (tức data ) n =
-        // a : filter : = (c_out, num_int): = (c_out, c_in*k1*k2)
-        // b : data : = (n , num_int) = (n, c_in*k1*k2)
-        // c output := (c_out, n)
   //batch index
   const int n_local = threadIdx.x + blockIdx.x * blockDim.x;
   // column index
   const int idx_int_num_local = threadIdx.y;
   const int c_channel =  blockIdx.y;
   if (n_local < n & idx_int_num_local < l & c_channel < c_out){
-    //  b[idx_int_num_local][n_local] = 1;
-      // auto b_element = b[n_local][idx_int_num_local];
-    // c[c_channel][idx_int_num_local][n_local] = 1;
-// c[c_channel][idx_int_num_local][n_local] = b[0][0];
-// c[c_channel][idx_int_num_local][n_local] = b[n_local][idx_int_num_local];
 ///
 // Đây là hàm xor, để tính xnor = k-nor;
-// c[c_channel][idx_int_num_local][n_local] = k - 2*__popc((unsigned int) a[c_channel][idx_int_num_local]^ (unsigned int) b[n_local][idx_int_num_local]);
 c[c_channel][idx_int_num_local][n_local] = __popc( (unsigned int) a[c_channel][idx_int_num_local]^ (unsigned int) b[n_local][idx_int_num_local]);
-// c[c_channel][idx_int_num_local][n_local] = __popc(12344);
-
-// auto rere = __popc(a[c_channel][idx_int_num_local]^b[n_local][idx_int_num_local]);
-
-    // for (idx = 0; idx<c_out; idx++){
-    //   const auto b_element = b[n_local][idx_int_num_local];
-    //     // c[idx][idx_int_num_local][n_local] = __popc(a[idx][idx_int_num_local]^b[n_local][idx_int_num_local]);
-    //       //  auto CC = __popc(a[idx][idx_int_num_local]^b[n_local][idx_int_num_local]);
-    //       // auto zz = a[idx][idx_int_num_local];
-    //       // auto zz = b[n_local][idx_int_num_local];
-    //       // a[idx][idx_int_num_local] = 1;
-    //       // b[idx_int_num_local][n_local] = 1;
-    //       // c[idx][idx_int_num_local][n_local] = 1;
-    //         // c[idx][idx_int_num_local][n_local] = a[idx][idx_int_num_local]+b[n_local][idx_int_num_local];
-    //         // c[idx][idx_int_num_local][n_local] = a[idx][idx_int_num_local];
-    //         // c[idx][idx_int_num_local][n_local] = b[n_local][idx_int_num_local];
-    //         c[idx][idx_int_num_local][n_local] = b_element;
-    // }
   }
 }
 
@@ -142,35 +102,20 @@ __global__ void get_final_result_kernel(
   if (n_local < n & c_channel < c_out){
         for (idx_l = 0; idx_l <l;idx_l++){
             output_bin_final[c_channel][n_local] += output_bin[c_channel][idx_l][n_local];
-            // output_bin_final[c_channel][n_local] += 1;
         }
         output_bin_final[c_channel][n_local] = k-2*output_bin_final[c_channel][n_local];
-        // output_bin_final[c_channel][n_local] = 1;
     }
 }
 
 torch::Tensor binary_gemm(torch::Tensor a, torch::Tensor b,  int c_out, int l, int k, int n, int transb, int alpha, int beta){
 
-//     b : columns_binary
-//     auto A = a.data_ptr<int>();
-//     auto B = b.data_ptr<int>();
-//     auto C = c.data_ptr<float>();
 
-//     float *D = alpha? THCudaTensor_data(state, alphas) : NULL;s
-//     cudaStream_t stream = THCState_getCurrentStream(state);
 //  k số hàng
 // n số cột
-//     binary_gemm_cuda(A, B, C, c_out, k, n, transb, alpha, beta);
-//     dim3 blockDim(BLOCK_DIM, BLOCK_DIM);
-//     dim3 gridDim(k/ENCODE_BITS+1, n/ENCODE_BITS+1, 1);
-    // const int kk = 1+(k-1)/ENCODE_BITS;
-    // const int threads = 1024;
+
     const dim3 threads(256, l/1+1);
     const dim3 blocks(n/256+1, c_out);
     torch::Tensor c = torch::zeros(torch::IntArrayRef({c_out,l,n}),torch::TensorOptions().dtype(torch::kInt32).device(torch::kCUDA, 0));
-    // std::cout << b.size(0) << "  ,   " << b.size(1) << '\n';
-    // std::cout << "b[0][0] : " << b[0][0] << '\n';
-    // std::cout << "a[0][0] : " << a[0][0] << '\n';
     AT_DISPATCH_ALL_TYPES(b.type(), "binary_gemm binary_gemm_kernel", ([&] {
     binary_gemm_kernel<scalar_t><<<blocks, threads>>>(
      a.packed_accessor32<scalar_t,2,torch::RestrictPtrTraits>(),
@@ -190,7 +135,5 @@ torch::Tensor binary_gemm(torch::Tensor a, torch::Tensor b,  int c_out, int l, i
     c_out ,n, l,k
     );
     }));
-//    return output;
-//     return c.transpose(0, 1);
     return d;
 }
